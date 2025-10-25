a8788e01bb686197a4ec8b60d188df3c7e32e955
SaqJun <bdhdvzg@gmail.com>
2025-10-25 16:58:10 +0530

    making changes

--- resources/js/script.js
+++ resources/js/script.js
@@ -3,6 +3,8 @@ const sidebar = document.getElementById("sidebar");
 const overlay = document.getElementById("overlay");
 const links = document.getElementById("sidebar-links");
 const submit =document.getElementById("bookBtn")
+const removeBtn = document.getElementsByClassName("remove-btn")
+const addBtn = document.getElementsByClassName("add-btn")
 
 // Functions
 function isEmail(email) {
@@ -39,6 +41,108 @@ overlay.onclick = () => {
     sidebar.classList.remove("active");
     overlay.classList.remove("active");
 };
+
+
+// Cart functionality
+let cart = [];
+let services = {
+    service1: {
+        name: "Dry Cleaning",
+        price: 200.00
+    },
+    service2: {
+        name: "Wash & Fold",
+        price: 100
+    },
+    service3: {
+        name: "Ironing",
+        price: 30
+    },
+    service4: {
+        name: "Stain Removal",
+        price: 500
+    },
+    service5: {
+        name: "Leather & Suede Cleaning",
+        price: 999
+    },
+    service6: {
+        name: "Wedding Dress Cleaning",
+        price: 2800
+    }
+}
+
+const serviceList = document.getElementById("services-list");
+const cartBody = document.querySelector(".cart .styled-table tbody");
+const cartFooter = document.querySelector(".cart .styled-table tfoot td strong");
+
+for (const key in services) {
+    const service = services[key];
+    const div = document.createElement('div');
+    div.classList.add('service-item')
+    div.innerHTML = `
+      <span>${service.name} - <span class="pricing">₹${service.price}</span></span>
+    `;
+    const button = document.createElement('button')
+    button.classList.add('btn', 'add-btn');
+    button.textContent = "Add to cart"
+
+    button.onclick = () => {
+        if (button.classList.contains('add-btn')){
+            addToCart(service)
+            button.classList.remove('add-btn')
+            button.classList.add('remove-btn')
+            button.textContent = "Remove item"
+        } else {
+            removeFromCart(service)
+            button.classList.remove('remove-btn')
+            button.classList.add('add-btn')
+            button.textContent = "Add to Cart"
+        }
+    }
+    div.appendChild(button)
+    serviceList.appendChild(div);
+}
+
+function addToCart(service) {
+    cart.push(service);
+    console.log(cart)
+    updateCart()
+}
+function removeFromCart(service) {
+    const index = cart.findIndex(item => item.name === service.name);
+    if (index > -1) cart.splice(index, 1);
+    console.log(cart);
+    updateCart()
+}
+
+// Cart modifications
+
+function updateCart() {
+  cartBody.innerHTML = "";
+  let total = 0;
+
+  if (cart.length === 0) {
+    const emptyRow = document.createElement("tr");
+    emptyRow.innerHTML = `<td colspan="3" style="text-align:center;">No items added</td>`;
+    cartBody.appendChild(emptyRow);
+    cartFooter.textContent = "₹0.00";
+    return;
+  }
+
+  cart.forEach((service, index) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${index + 1}</td>
+      <td>${service.name}</td>
+      <td>₹${service.price.toFixed(2)}</td>
+    `;
+    cartBody.appendChild(row);
+    total += service.price;
+  });
+  const totalPrice = document.getElementById('total-price')
+  totalPrice.textContent = `₹${total.toFixed(2)}`;
+}
 
 // Email Sending functionality
 function sendMail(name, email) {
@@ -90,6 +194,11 @@ submit.onclick = () => {
         if (!isPhoneNumber(phoneNumber)) {
             errorMsg += "Phone Number\n"
         }
+    }
+
+    //Validating the cart
+    if (cart.length === 0) {
+        emptyFields += "The cart is empty\n"
     }
 
     // Structuring the alert messages
